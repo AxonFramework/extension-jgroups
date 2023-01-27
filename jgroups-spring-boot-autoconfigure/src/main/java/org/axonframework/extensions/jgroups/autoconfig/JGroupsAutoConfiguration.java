164810ce62f918dbe1c382e185f086dfd522ffee
Gerard Klijs <gerard.klijs@axoniq.io>
2023-01-27 13:44:38 +0100

    Add support for Spring Boot 3 autoconfiguration.

--- jgroups-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/jgroups/autoconfig/JGroupsAutoConfiguration.java
+++ jgroups-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/jgroups/autoconfig/JGroupsAutoConfiguration.java
@@ -1,27 +1,11 @@
 /*
- * Copyright (c) 2010-2018. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Copyright (c) 2010-2018. Axon Framework
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -47,6 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -55,13 +40,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Configuration
+@AutoConfiguration
 @AutoConfigureAfter(RoutingStrategyAutoConfiguration.class)
 @AutoConfigureBefore(InfraConfiguration.class)
 @EnableConfigurationProperties(DistributedCommandBusProperties.class)
--- jgroups-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/jgroups/autoconfig/RoutingStrategyAutoConfiguration.java
+++ jgroups-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/jgroups/autoconfig/RoutingStrategyAutoConfiguration.java
@@ -1,19 +1,17 @@
 /*
- * Copyright (c) 2010-2017. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- *
  */
 
 package org.axonframework.extensions.jgroups.autoconfig;
@@ -21,11 +19,11 @@ package org.axonframework.extensions.jgroups.autoconfig;
 import org.axonframework.commandhandling.distributed.AnnotationRoutingStrategy;
 import org.axonframework.commandhandling.distributed.RoutingStrategy;
 import org.axonframework.commandhandling.distributed.UnresolvedRoutingKeyPolicy;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Auto configuration for {@link RoutingStrategy}.
@@ -33,7 +31,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Milan Savic
  * @since 3.1.1
  */
-@Configuration
+@AutoConfiguration
 @AutoConfigureBefore(JGroupsAutoConfiguration.class)
 @ConditionalOnExpression("${axon.distributed.enabled:false}")
 public class RoutingStrategyAutoConfiguration {
