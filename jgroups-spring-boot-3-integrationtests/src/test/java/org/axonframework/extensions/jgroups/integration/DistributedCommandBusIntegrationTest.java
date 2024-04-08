a9d448cbdf542379cd2b806031ca5106143fadc5
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-04-08 11:50:23 +0200

    Adjust assertion to verify different span name
    Adjust assertion to verify different span name
    
    #294

--- jgroups-spring-boot-3-integrationtests/src/test/java/org/axonframework/extensions/jgroups/integration/DistributedCommandBusIntegrationTest.java
+++ jgroups-spring-boot-3-integrationtests/src/test/java/org/axonframework/extensions/jgroups/integration/DistributedCommandBusIntegrationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2023. Axon Framework
+ * Copyright (c) 2010-2024. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -126,7 +126,7 @@ class DistributedCommandBusIntegrationTest {
     private void verifySpans(SpanFactory spanFactory) {
         assertTrue(spanFactory instanceof TestSpanFactory);
         TestSpanFactory testSpanFactory = (TestSpanFactory) spanFactory;
-        testSpanFactory.verifySpanCompleted("DistributedCommandBus.dispatch");
+        testSpanFactory.verifySpanCompleted("CommandBus.dispatchDistributedCommand");
         testSpanFactory.verifySpanCompleted("JGroupsConnector.processDispatchMessage");
     }
 
